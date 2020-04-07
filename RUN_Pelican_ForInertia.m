801867b22875d00b2500d611503d54a985f231bb
Devin <devinbarcelos@gmail.com>
2020-04-07 19:24:41 -0400

    Adding comments to RUN_Pelican_ForInertia.m

--- RUN_Pelican_ForInertia.m
+++ RUN_Pelican_ForInertia.m
@@ -1,23 +1,47 @@
-% Run Pelican Datase to calculate moment of inertias
-clear,clc
-%%Solve for moment of inertia equations
+% Run Pelican Database to calculate moment of inertias from experiment
+% The intention with this is to determine if the moment of inertias from
+% being used are reasonable. 
 
-syms w_x w_y w_z
-syms M_x M_y M_z
-syms Ixx Iyy Izz
-syms wd_x wd_y wd_z
+% Code is 2 parts:
+%   Part 1 - Calculate the equations needed to solve for moment of inertia
+%            from the equations of motion. This is done in symbolic form.
+%
+%   Part 2 - Solve these equation using the predicted moment and the 
+%            measure flight test body rates.
+%
+%       NOTE: This make a bold decision that the prediction is perfect.
+%       This makes AeroMV NOT a soley predictive program and should be used
+%       only for testing purposes.
+%
+% D.F.B. in Barrie ON, Canada, Apr. 2020
+
+clear,clc
+
+%% Solve for moment of inertia equations
+
+syms w_x w_y w_z % Body rates
+syms M_x M_y M_z % Moments (N.m)
+syms Ixx Iyy Izz % Moment of inertias
+syms wd_x wd_y wd_z % Rotational accelertions
+
+% Equation of motion expanded from vector form to 3 eqn with 3 unknowns
+%      Notes: - The cross produce has already been executed manually
+%             - This assumes that the moment of inertias are symmetric as
+%             there are only 3 equations (ie. only Ixx, Iyy, Izz)
 
 eqn1 = (w_y*w_z*(Iyy - Izz)+M_x)/wd_x == Ixx;
 eqn2 = (w_z*w_x*(Izz - Ixx)+M_y)/wd_y == Iyy;
 eqn3 = (w_x*w_y*(Ixx - Iyy)+M_z)/wd_z == Izz;
 
+% Solve these equations
 S = solve([eqn1 eqn2 eqn3],[Ixx Iyy Izz],'ReturnConditions',true);
 
 % These are the resulting equations
 % Ixx = (M_x*wd_y*wd_z + M_y*w_y*w_z*wd_z - M_z*w_y*w_z*wd_y + M_x*w_x^2*w_y*w_z + M_y*w_x*w_y^2*w_z + M_z*w_x*w_y*w_z^2)/(wd_x*w_x^2*w_y*w_z + wd_y*w_x*w_y^2*w_z + wd_z*w_x*w_y*w_z^2 + wd_x*wd_y*wd_z);
 % Iyy = (M_y*wd_x*wd_z - M_x*w_x*w_z*wd_z + M_z*w_x*w_z*wd_x + M_x*w_x^2*w_y*w_z + M_y*w_x*w_y^2*w_z + M_z*w_x*w_y*w_z^2)/(wd_x*w_x^2*w_y*w_z + wd_y*w_x*w_y^2*w_z + wd_z*w_x*w_y*w_z^2 + wd_x*wd_y*wd_z);
 % Izz = (M_z*wd_x*wd_y + M_x*w_x*w_y*wd_y - M_y*w_x*w_y*wd_x + M_x*w_x^2*w_y*w_z + M_y*w_x*w_y^2*w_z + M_z*w_x*w_y*w_z^2)/(wd_x*w_x^2*w_y*w_z + wd_y*w_x*w_y^2*w_z + wd_z*w_x*w_y*w_z^2 + wd_x*wd_y*wd_z);
-
+%  NOTE: These equations were tested by solving for know Inertias and gave
+%  the correct values 
 
 %% Run AEROMV
 clear Ixx Iyy Izz
@@ -71,16 +95,24 @@ for i = begin:int:fin
     fprintf(strcat(num2str(i),' Complete\n'))
     
     %  -------- Calculate moment of inertias --------
-    
+    % Set moment components as M_B that is calculated from AeroMV
     M_x = OUTP(j).M_B(1); M_y = OUTP(j).M_B(2); M_z = OUTP(j).M_B(3);
+    % Set omega as the body_rates from the experiements
     w_x = BODY_RATES(i,1); w_y = BODY_RATES(i,2); w_z = BODY_RATES(i,3);
+    % Calculate rotation acceleration from the flight data using the
+    % frequency of the data. Assuming first order, linear (as does AeroMV)
     a = (BODY_RATES(i+1,:)- BODY_RATES(i,:)).*(STATE.FREQ);
     wd_x =a(1); wd_y = a(2); wd_z = a(3);
     
+    % Calculate the moment of inertias at the given data point
     Ixx(j) = (M_x*wd_y*wd_z + M_y*w_y*w_z*wd_z - M_z*w_y*w_z*wd_y + M_x*w_x^2*w_y*w_z + M_y*w_x*w_y^2*w_z + M_z*w_x*w_y*w_z^2)/(wd_x*w_x^2*w_y*w_z + wd_y*w_x*w_y^2*w_z + wd_z*w_x*w_y*w_z^2 + wd_x*wd_y*wd_z);
     Iyy(j) = (M_y*wd_x*wd_z - M_x*w_x*w_z*wd_z + M_z*w_x*w_z*wd_x + M_x*w_x^2*w_y*w_z + M_y*w_x*w_y^2*w_z + M_z*w_x*w_y*w_z^2)/(wd_x*w_x^2*w_y*w_z + wd_y*w_x*w_y^2*w_z + wd_z*w_x*w_y*w_z^2 + wd_x*wd_y*wd_z);
     Izz(j) = (M_z*wd_x*wd_y + M_x*w_x*w_y*wd_y - M_y*w_x*w_y*wd_x + M_x*w_x^2*w_y*w_z + M_y*w_x*w_y^2*w_z + M_z*w_x*w_y*w_z^2)/(wd_x*w_x^2*w_y*w_z + wd_y*w_x*w_y^2*w_z + wd_z*w_x*w_y*w_z^2 + wd_x*wd_y*wd_z);
 end
 
-I_mean = [mean(Ixx(Ixx<1)) mean(Iyy(Iyy<1)) mean(Izz(Izz<1))];
+% Calculate the mean moment of inertia, cutting off any strange data points
+% The cutoff criteria is set to 1 but this could be modified with
+% justification
+cutoff = 1; 
+I_mean = [mean(Ixx(Ixx<cutoff)) mean(Iyy(Iyy<cutoff)) mean(Izz(Izz<cutoff))];
 save('InertiaData_Flight40')
