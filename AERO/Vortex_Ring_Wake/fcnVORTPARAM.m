5ba2abe77d16da3f387410ffce2458214d562e57
Devin <devinbarcelos@gmail.com>
2020-05-19 14:27:43 -0400

    Adding function to compare to J. Tsaltas. Fixed vortparam calcs with a couple typos. Issue now lies with moving wave within x_y plane in fcnRINGGEOM.m

--- AERO/Vortex_Ring_Wake/fcnVORTPARAM.m
+++ AERO/Vortex_Ring_Wake/fcnVORTPARAM.m
@@ -5,7 +5,7 @@ function [VORTPARAM] = fcnVORTPARAM(N_b,RPM,V_inf,T,rho,R)
 
 %% Calculate vortex ring parameters
 % Calculate circulation
-VORTPARAM.circ = 2*T./(rho*R^2*N_b*(RPM/60)*pi);
+VORTPARAM.circ = T./(rho*R^2*N_b*(RPM/60)*pi);
 
 % Momentum theory
 mom = 2*T./(rho.*pi.*(R.^2));
@@ -15,7 +15,7 @@ VORTPARAM.z = (1./(2*N_b.*(RPM/60))).*(V_inf(:,3)+sqrt(V_inf(:,3).^2+mom));
 
 % Skew angle
 V_xy = sqrt(V_inf(:,1).^2+V_inf(:,2).^2);
-VORTPARAM.chi = atan(V_xy./(0.5*V_inf(:,3)+sqrt(V_inf(:,3).^2+mom)));
+VORTPARAM.chi = atan(V_xy./(0.5*(V_inf(:,3)+sqrt(V_inf(:,3).^2+mom))));
 
 end
 
--- AERO/Vortex_Ring_Wake/runVORTRINGMAIN.m
+++ AERO/Vortex_Ring_Wake/runVORTRINGMAIN.m
@@ -1,8 +1,7 @@
 % An example run script for the Vortex Ring Wake model
 clear,clc
-num_seg = 10;
+num_seg = 100;
 num_ring = 5;
-
 
 GEOM.ROTCENTER = [0.5 0 0;0 0.5 0; -0.5 0 0; 0 -0.5 0];
 GEOM.N_b = 2; % number of blades
@@ -10,7 +9,7 @@ GEOM.R = 0.127;
 % Calculate circulation
 
 
-COND.omega = [4000 4500 4222 7546]';
+COND.RPM = [4000 4500 4222 7546]';
 COND.T = [5 4 2.5 7.5]';
 COND.rho = 1.225;
 COND.V_inf = [0.0926    0.0454    0.1133;
@@ -19,3 +18,18 @@ COND.V_inf = [0.0926    0.0454    0.1133;
     0.0926    0.0488    0.1133];
 
 [matQ,VORTPARAM] = fcnVORTRINGMAIN(num_seg,num_ring,GEOM,COND);
+
+
+
+%% Comparing to J. Tsaltas' Fast Multirotor Performance Prediction wake model
+% folder = pwd;
+% 
+% cd('C:\Users\Devin\Documents\FMVP_FastMultirotorPerformancePrediction')
+% vel_mag = sqrt(COND.V_inf(:,1).^2+COND.V_inf(:,2).^2+COND.V_inf(:,3).^2);
+% AOA_R = acosd(dot(COND.V_inf',repmat([0 0 1]',1,size(COND.V_inf,1)))'./(vel_mag));
+% 
+% vel_mag = permute(vel_mag,[3,2,1]);
+% AOA_R = permute(AOA_R,[3,2,1]);
+% 
+% [vi_int,vi_self,skewRAD,wi] = fcnWIM(COND.rho,4,GEOM.N_b,GEOM.R*2,GEOM.ROTCENTER,permute(COND.T,[3 2 1]),permute(COND.RPM,[3 2 1]),AOA_R,vel_mag);
+% cd(folder)
