3c09a020d05f288927ae6dd10986196478d1b6b6
Devin <devinbarcelos@gmail.com>
2021-03-23 12:49:09 -0400

    Adding more iterations in RUN BEMTVELDISDT for rolling rotor investigations

--- RUN/RUN_BEMTVELDIST.m
+++ RUN/RUN_BEMTVELDIST.m
@@ -5,15 +5,17 @@ cd(dir(1:end-17))
 fcnRUN_DIR()
 
 % Values to iterate through:
-mu =  [0.069622 0.13924 0.20886];
-roll_rate = -1:0.2:1;
+mu = [0.0464 0.1392];
+roll_rate = -1:0.5:1;
+AOA_R_sweep = [0 5 15];
+matLOCATION_sweep = [0 0 0; 0 0.24 0];
 
 rotor_radius = 0.2286;
 
 % % Creat GEOM structure
 GEOM.ROTOR.strNAME = 'Tmotor';
 % GEOM.ROTOR.strNAME = 'APC_10x_4_7_Updated';
-GEOM.ROTOR.matLOCATION = [0 0 0];
+
 GEOM.ROTOR.matROT = -1;
 
 % Air structure
@@ -21,17 +23,46 @@ AIR.density = 1.225;
 AIR.kinvisc = 1.46E-5;
 
 % State structure
-STATE.RPM = 3000;
-STATE.AOA_R = 0;
+STATE.RPM = 4500;
 
+count = 0;
 PERF.Temp = [];
-for i = 1:length(mu)
-    STATE.VEL_ROTOR_MAG = mu(i)*(STATE.RPM/60*2*pi)*rotor_radius;
-    
-    for j = 1:length(roll_rate)
-        STATE.BODY_RATES = [roll_rate(j) 0 0];
-        
-       	TEMP = fcnRUNBEMT_VELDIST(GEOM, AIR, PERF, STATE);
-        DATA(i,j) = TEMP.ROTOR;
+for i = 1:size(matLOCATION_sweep,1)
+    GEOM.ROTOR.matLOCATION = matLOCATION_sweep(i,:);
+    for j = 1:length(AOA_R_sweep)
+        STATE.AOA_R = AOA_R_sweep(j);
+        for k = 1:length(mu)
+            STATE.VEL_ROTOR_MAG = mu(i)*(STATE.RPM/60*2*pi)*rotor_radius;
+            
+            for m = 1:length(roll_rate)
+                STATE.BODY_RATES = [roll_rate(m) 0 0];
+                
+                TEMP = fcnRUNBEMT_VELDIST(GEOM, AIR, PERF, STATE);
+                count = count + 1;
+
+                DATA_temp.RUN_NUM = count;
+                DATA_temp.YAxis_offset = GEOM.ROTOR.matLOCATION(2);
+                DATA_temp.TPP_Angle = STATE.AOA_R;
+                DATA_temp.Velocity = STATE.VEL_ROTOR_MAG;
+                DATA_temp.RPM = STATE.RPM;
+                DATA_temp.Roll_Rate = STATE.BODY_RATES(1);
+                DATA_temp.mu = mu(k);
+                DATA_temp.density = AIR.density;
+                DATA_temp.kinvisc = AIR.kinvisc;
+                DATA_temp.RotorRadius = rotor_radius;
+                DATA_temp.CT = TEMP.ROTOR.CT;
+                DATA_temp.CP = TEMP.ROTOR.CP;
+                DATA_temp.CQ = TEMP.ROTOR.CQ;
+                DATA_temp.CMx = TEMP.ROTOR.CMx;
+                DATA_temp.CMy = TEMP.ROTOR.CMy;
+                DATA_temp.CFx = TEMP.ROTOR.CNx;
+                DATA_temp.CFy = TEMP.ROTOR.CNy;
+                DATA_temp.RAW_BEMT_RESULTS = TEMP.ROTOR;
+            
+                DATA(:,count) = DATA_temp;
+                
+            end
+        end
     end
-end 
+end
+
